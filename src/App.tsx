886ddc4b114d408f24b0f40a3c710de4ad634f52
ksrichard <bertok.richard.94@gmail.com>
2026-02-01 22:43:02 +0100

    style changes

--- src/App.tsx
+++ src/App.tsx
@@ -28,8 +28,7 @@ const App: React.FC = () => {
     };
 
     return (
-        <div
-            className="overflow-hidden bg-gradient-to-b from-gray-900 via-blue-950 to-gray-900 text-white min-h-screen">
+        <div className="overflow-hidden bg-gradient-to-b from-gray-900 via-blue-950 to-gray-900 text-white min-h-screen">
                     <HeroSection scrollY={scrollY} onGetStartedClick={scrollToFeatures}
                                  onGetNotifiedClick={scrollToSubscribe}/>
                     <FeaturesSection features={features} featuresRef={featuresRef}/>
